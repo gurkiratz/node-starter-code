534be1ad36be77e55c9b607f7198b9782f4add46
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2023-04-28 16:40:04 +0530

    Auth: Generate Password Reset Token

--- 4-natours/starter/models/UserModel.js
+++ 4-natours/starter/models/UserModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -35,6 +36,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -70,6 +73,22 @@ userSchema.methods.changedPasswordAfter = async function (JWTtimestamp) {
 
   // False means NOT changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  // weird method of hashing resetToken
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // in ms
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
