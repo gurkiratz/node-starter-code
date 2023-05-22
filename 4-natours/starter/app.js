deb1381dd80dd3f5b764d60d48560ab62d3df134
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2023-05-22 17:29:06 +0530

    Auth: Set Security HTTP Headers
    Via Helmet Package

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,22 +11,30 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000, // 60 Minutes ~ 1 Hour
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`)); // To serve static files such as images, CSS files, and JavaScript files
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
